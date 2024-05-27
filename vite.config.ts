1fc69dc3b629102067e8248e8c554be1ece8fa41
Yunus ÖLMEZ <yns4832@gmail.com>
2024-05-27 16:25:05 +0200

    correct the build

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import dotenv from 'dotenv';
+dotenv.config()
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
